6ccc2f9947ae211da3fa277cd69d4c4308032f0c
Trend20 <enockomondi305@gmail.com>
2024-10-01 11:00:47 +0300

    adding swagger config for documentation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { setupSwagger } from './common/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
   // applies security hardening settings. using defaults: https://www.npmjs.com/package/helmet
   app.use(helmet());
   app.setGlobalPrefix('api/v1');
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
